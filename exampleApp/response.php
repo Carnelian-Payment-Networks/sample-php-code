4c41dbe5c36ead6a65d1dd6fb832a615d583280c
Ashish Waingankar <ashish@paynet.co.in>
2020-06-17 20:07:28 +0530

    Initial Release

---
+++ exampleApp/response.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'paynet.php';
+
+$paynet = new paynet("email@example.com", "vZ8nRlhEg3vAsEeOWTtmgC4Uh8OAsWiXWqkNMCs2GYD9CSXzLrV130Wl9LWBHezDETrrvUtcS7luZi3KJZMN0I89t5LbjkOYefpK");
+$result = $paynet->verify_payment($_POST['payment_reference']);
+echo "<center><h1>" . $result->result . "</h1></center>";
+
+?>
